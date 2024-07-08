90a728eede7fccb161909d7096dff5045f92504a
Elias Rangel <eli.rangel@atscale.com>
2024-07-08 10:34:42 -0400

    fix: fixed script and added translated logs

--- functions/parse-logs.js
+++ functions/parse-logs.js
@@ -1,77 +1,120 @@
 import fs from "fs";
 import path from "path";
 
-// type jsonObjectExpectedType = {
-//   modelName: string;
-//   catalogName: string;
-//   userQuestion: string;
-//   engineResponse: {
-//     "query-id": string;
-//     metadata: { succeeded: boolean; "error-message": string };
-//   };
-//   inboundQuery: string;
-//   cleanedUpSql: string;
-// };
-// type jsonTranslatedType = {
-//   modelName: string;
-//   catalogName: string;
-//   userQuestion: string;
-//   inboundQuery: string;
-//   cleanedUpSql: string;
-//   errorMessage: string;
-// };
+const extractValue = (arr, key, valueKey) => {
+  const tmp = arr.reduce((accumulator, jsonObject) => {
+    if (accumulator[key(jsonObject)])
+      return {
+        ...accumulator,
+        [key(jsonObject)]: [
+          ...accumulator[key(jsonObject)],
+          valueKey(jsonObject),
+        ],
+      };
+    return {
+      ...accumulator,
+      [key(jsonObject)]: [valueKey(jsonObject)],
+    };
+  }, {});
+
+  return tmp;
+};
 
 export const parseLogs = async (logDirectory) => {
   const moveTo = path.join(logDirectory, "..", "translated_docs");
-
+  const fullPathFolder = path.join(moveTo, "full");
+  const questionPathFolder = path.join(moveTo, "question-error");
+  const inboundPathFolder = path.join(moveTo, "inboundQuery-error");
   fs.mkdir(moveTo, (err) => {
-    if (!err.code === "EEXIST") {
+    if (err && Object.hasOwn(err, "code") && !err.code === "EEXIST") {
       throw err;
     } else {
-      console.log("FOLDER EXISTS");
+      console.log("fullPath FOLDER EXISTS");
+    }
+  });
+  fs.mkdir(fullPathFolder, (err) => {
+    if (err && Object.hasOwn(err, "code") && !err.code === "EEXIST") {
+      throw err;
+    } else {
+      console.log("fullPath FOLDER EXISTS");
+    }
+  });
+  fs.mkdir(questionPathFolder, (err) => {
+    if (err && Object.hasOwn(err, "code") && !err.code === "EEXIST") {
+      throw err;
+    } else {
+      console.log("questionPath FOLDER EXISTS");
+    }
+  });
+  fs.mkdir(inboundPathFolder, (err) => {
+    if (err && Object.hasOwn(err, "code") && !err.code === "EEXIST") {
+      throw err;
+    } else {
+      console.log("inboundPath FOLDER EXISTS");
     }
   });
   try {
     const files = await fs.promises.readdir(logDirectory);
-
     for (const file of files) {
       const fromPath = path.join(logDirectory, file);
-      const toPath = path.join(moveTo, file);
+
+      const fullPath = path.join(fullPathFolder, file);
+      const questionPath = path.join(questionPathFolder, file);
+      const inboundQuery = path.join(inboundPathFolder, file);
+
       const status = await fs.promises.stat(fromPath);
+
       if (status.isFile()) {
-        const json = JSON.parse(
-          (await fs.promises.readFile(fromPath)).toString()
-        );
-        const arr = json.reduce((accumulator, jsonObject) => {
-          if (accumulator[jsonObject.engineResponse.metadata["error-message"]])
-            return {
-              ...accumulator,
-              [jsonObject.engineResponse.metadata["error-message"]]: [
-                ...accumulator[
-                  jsonObject.engineResponse.metadata["error-message"]
-                ],
-                {
-                  ...jsonObject,
-                  errorMessage:
-                    jsonObject.engineResponse.metadata["error-message"],
-                },
-              ],
+        const f = (await fs.promises.readFile(fromPath)).toString();
+
+        const json = JSON.parse(f);
+        const arr = extractValue(
+          json,
+          (obj) => obj.engineResponse.metadata["error-message"],
+          (obj) => {
+            const tmp = {
+              modelName: obj.modelName,
+              catalogName: obj.catalogName,
+              userQuestion: obj.userQuestion,
+              inboundQuery: obj.inboundQuery,
+              cleanedUpSql: obj.cleanedUpSql,
+              errorMessage: obj.engineResponse.metadata["error-message"],
             };
-          return {
-            ...accumulator,
-            [jsonObject.engineResponse.metadata["error-message"]]: [
-              {
-                ...jsonObject,
-                errorMessage:
-                  jsonObject.engineResponse.metadata["error-message"],
-              },
-            ],
-          };
-        }, {});
-        console.log(arr);
+
+            return tmp;
+          }
+        );
+        const questionErrorArray = extractValue(
+          json,
+          (obj) => obj.engineResponse.metadata["error-message"],
+          (obj) => {
+            const tmp = {
+              userQuestion: obj.userQuestion,
+            };
+
+            return tmp;
+          }
+        );
+        const questionInboundQueryArray = extractValue(
+          json,
+          (obj) => obj.engineResponse.metadata["error-message"],
+          (obj) => {
+            const tmp = {
+              inboundQuery: obj.inboundQuery,
+            };
+
+            return tmp;
+          }
+        );
+
+        await fs.promises.writeFile(fullPath, JSON.stringify(arr, null, 2));
         await fs.promises.writeFile(
-          toPath,
-          JSON.stringify(arr, null, 2, { mode: "w" })
+          questionPath,
+          JSON.stringify(questionErrorArray, null, 2)
+        );
+        await fs.promises.writeFile(
+          inboundQuery,
+          JSON.stringify(questionInboundQueryArray, null, 2)
         );
       }
     }
